7e7a92034f5c2d75623df1ab9db8ec2ac335844b
realwahyuputra <realwahyuputra@gmail.com>
2025-04-14 09:48:19 +0700

    Update date-binning.ts

--- app/components/sidebar/date-binning.ts
+++ app/components/sidebar/date-binning.ts
@@ -31,11 +31,11 @@ export function binDates(_list: ChatHistoryItem[]) {
 
 function dateCategory(date: Date) {
   if (isToday(date)) {
-    return 'Today';
+    return 'Hari Ini';
   }
 
   if (isYesterday(date)) {
-    return 'Yesterday';
+    return 'Kemarin';
   }
 
   if (isThisWeek(date)) {
@@ -46,7 +46,7 @@ function dateCategory(date: Date) {
   const thirtyDaysAgo = subDays(new Date(), 30);
 
   if (isAfter(date, thirtyDaysAgo)) {
-    return 'Past 30 Days';
+    return 'Dalam 30 Hari';
   }
 
   if (isThisYear(date)) {
