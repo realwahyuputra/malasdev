3ae0a5a1002ddabab2a252826e845e212145eae7
realwahyuputra <realwahyuputra@gmail.com>
2025-04-14 18:40:58 +0700

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -317,8 +317,8 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
               <div id="intro" className="mt-[8vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                <div className="inline-block bg-blue-500 rounded-full px-4 py-2 mb-4 animate-fade-in">
-                  <span className="text-white font-medium text-sm lg:text-base">Tanpa Limit Token 🔥</span>
+                <div className="inline-block bg-blue-500 rounded-full px-6 py-2 mb-4 animate-fade-in">
+                  <span className="text-white font-medium text-sm">Tanpa Limit Token 🔥</span>
                 </div>
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
                   Enggan Ngoding?
