c43a7fbcc1b93366c1b1c7059d45cb7681fe931e
realwahyuputra <realwahyuputra@gmail.com>
2025-04-14 19:32:58 +0700

    Update Header.tsx

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -30,7 +30,7 @@ export function Header() {
       </div>
       {chat.started && ( // Display ChatDescription and HeaderActionButtons only when the chat has started.
         <>
-          <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
+          <span className="sm:hidden flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
             <ClientOnly>{() => <ChatDescription />}</ClientOnly>
           </span>
           <ClientOnly>
